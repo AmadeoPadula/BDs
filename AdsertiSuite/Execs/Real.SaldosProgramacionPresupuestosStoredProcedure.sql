55b283ae55831b1548932ccebf48f462938a89e0
hoxbit <arthur.kovec@gmail.com>
2017-05-15 15:37:01 -0500

    Carga inicio parcial

--- AdsertiSuite/Execs/Real.SaldosProgramacionPresupuestosStoredProcedure.sql
+++ AdsertiSuite/Execs/Real.SaldosProgramacionPresupuestosStoredProcedure.sql
@@ -4,10 +4,14 @@ DECLARE
 	@Fecha DATE,
 	@Maestro BIT--DEFINICION DE LA VISTA QUE SE QUIERE MOSTAR (MAESTRO/DETALLE)
 
-SET @AreaId = 19
-SET @EtapaDesarrolloId = 1
+SET @AreaId = 13
+SET @EtapaDesarrolloId = 2
 SET @Fecha = GETDATE()
---SET @Maestro = 0
-SET @Maestro = 1
+SET @Maestro = 0
+--SET @Maestro = 1
 					
 EXEC Real.SaldosProgramacionPresupuestosStoredProcedure @AreaId, @EtapaDesarrolloId, @Fecha, @Maestro
+
+
+--SELECT EtapaDesarrolloId,* FROM Real.Presupuestos WHERE EtapaDesarrolloId IS NOT NULL--PresupuestoId = 1152
+--SELECT EtapaDesarrolloId,* FROM Real.Presupuestos WHERE PresupuestoId = 1201
